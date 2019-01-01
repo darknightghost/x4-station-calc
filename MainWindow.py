74bb0d0f9b9a0ecdd8a0d828f0bf541a5fdb8ee1
wang_siyuan <darknightghost@gmail.com>
2019-01-01 17:02:52 +0800

    UTC:2019-01-01  9:02:52 : menu

--- MainWindow.py
+++ MainWindow.py
@@ -15,20 +15,137 @@
 '''
 
 import PyQt5
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QMenu, QAction, QActionGroup
 
 import StringTable
+import pathlib
+import json
+import locale
 
 
 class MainWindow(QMainWindow):
+    CONFIG_PATH = pathlib.Path(__file__).parent / ".config"
+
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle(StringTable.get_string("MAIN_WINDOW_TITLE"))
+        self.__load_window()
+
+        #Language
+        try:
+            StringTable.set_locale(self.__config["locale"])
+
+        except KeyError:
+            sys_locale = locale.getdefaultlocale()[0]
+            try:
+                StringTable.set_locale(sys_locale)
+            except KeyError:
+                pass
+
+            self.__config["locale"] = StringTable.locale()
+
+        self.setWindowTitle(StringTable.get_string("TITLE_MAIN_WINDOW"))
         desktop = QApplication.desktop()
         self.resize(desktop.width() * 3 / 4, desktop.height() * 3 / 4)
         self.move(desktop.width() / 8, desktop.height() / 8)
+        self.__init_file_menu()
+        self.__init_setting_menu()
+        self.__init_view_menu()
+
+    #Initialize menus
+    def __init_file_menu(self):
+        self.__file_menu = QMenu(StringTable.get_string("MENU_FILE"))
+        self.menuBar().addMenu(self.__file_menu)
+        self.__file_open_action = QAction(
+            StringTable.get_string("MENU_FILE_OPEN"))
+        self.__file_open_action.triggered.connect(self.on_menu_file_open)
+        self.__file_menu.addAction(self.__file_open_action)
+
+        self.__file_menu.addSeparator()
+        self.__file_save_action = QAction(
+            StringTable.get_string("MENU_FILE_SAVE"))
+        self.__file_save_action.triggered.connect(self.on_menu_file_save)
+        self.__file_menu.addAction(self.__file_save_action)
+        self.__file_save_as_action = QAction(
+            StringTable.get_string("MENU_FILE_SAVE_AS"))
+        self.__file_save_as_action.triggered.connect(self.on_menu_file_saveas)
+        self.__file_menu.addAction(self.__file_save_as_action)
+
+        self.__file_menu.addSeparator()
+        self.__file_close_action = QAction(
+            StringTable.get_string("MENU_FILE_CLOSE"))
+        self.__file_close_action.triggered.connect(self.on_menu_file_close)
+        self.__file_menu.addAction(self.__file_close_action)
+
+    def __init_setting_menu(self):
+        self.__setting_menu = QMenu(StringTable.get_string("MENU_SETTING"))
+        self.menuBar().addMenu(self.__setting_menu)
+        self.__setting_language_menu = QMenu(
+            StringTable.get_string("MENU_SETTING_LANGUAGE"))
+        self.__setting_menu.addMenu(self.__setting_language_menu)
+
+        #Languages
+        self.__language_action_group = QActionGroup(self)
+        languages = StringTable.local_dict()
+        for l in languages.keys():
+            action = QAction(languages[l], self.__language_action_group)
+            setattr(action, "locale", l)
+            action.setCheckable(True)
+            action.setChecked(l == StringTable.locale())
+            self.__setting_language_menu.addAction(action)
+
+        self.__language_action_group.triggered[QAction].connect(
+            self.on_change_locale)
+
+    def __init_view_menu(self):
+        self.__view_menu = QMenu(StringTable.get_string("MENU_VIEW"))
+        self.menuBar().addMenu(self.__view_menu)
+
+        self.__view_module_list_menu = QAction(
+            StringTable.get_string("MENU_VIEW_MODULE_LIST"))
+        self.__view_menu.addAction(self.__view_module_list_menu)
+
+        self.__view_module_info_menu = QAction(
+            StringTable.get_string("MENU_VIEW_MODULE_INFO"))
+        self.__view_menu.addAction(self.__view_module_info_menu)
+
+    def __save_window(self):
+        s = json.dumps(self.__config)
+        with open(str(self.CONFIG_PATH), "w") as f:
+            f.write(s)
+
+    def __load_window(self):
+        if self.CONFIG_PATH.exists():
+            with open(str(self.CONFIG_PATH)) as f:
+                self.__config = json.loads(f.read())
+
+        else:
+            self.__config = {}
 
     #Slots
+    def on_change_locale(self, action):
+        self.__config["locale"] = action.locale
+        QMessageBox.information(
+            self, StringTable.get_string("TITLE_INFO"),
+            StringTable.get_string("INFO_EFFECT_NEXT_LAUNCH"))
+
+    def on_menu_file_open(self, state):
+        pass
+
+    def on_menu_file_save(self, state):
+        pass
+
+    def on_menu_file_saveas(self, state):
+        pass
+
+    def on_menu_file_close(self, state):
+        self.close()
+
+    #Events
+    def closeEvent(self, event):
+        self.__save_window()
+        super().closeEvent(event)
 
     #Methods
     def open_station(self, path):
--- StringTable.py
+++ StringTable.py
@@ -17,12 +17,74 @@
     UI strings.
 '''
 
-__LOCALE = 44
-__DEFRAULT = 44
+__LOCALE = "en_US"
+__DEFAULT = "en_US"
+__LOCALES = {"en_US": "English(American)", "zh_CN": "简体中文"}
 __STRING_TABLE = {
-    "MAIN_WINDOW_TITLE": {
-        44: "X4 Station Calculator",
-        86: "X4 空间站模块计算工具"
+    #Titles
+    "TITLE_MAIN_WINDOW": {
+        "en_US": "X4 Station Calculator",
+        "zh_CN": "X4 空间站模块计算工具"
+    },
+    "TITLE_MODULE_LIST": {
+        "en_US": "Station Modules",
+        "zh_CN": "空间站模块"
+    },
+    "TITLE_MODULE_INFO": {
+        "en_US": "Module Info",
+        "zh_CN": "模块信息"
+    },
+    "TITLE_INFO": {
+        "en_US": "Info",
+        "zh_CN": "提示"
+    },
+
+    #Menu
+    "MENU_FILE": {
+        "en_US": "&File",
+        "zh_CN": "文件(&F)"
+    },
+    "MENU_FILE_OPEN": {
+        "en_US": "&Open",
+        "zh_CN": "打开(&O)"
+    },
+    "MENU_FILE_SAVE": {
+        "en_US": "&Save",
+        "zh_CN": "保存(&S)"
+    },
+    "MENU_FILE_SAVE_AS": {
+        "en_US": "&Save as",
+        "zh_CN": "另存为(&A)"
+    },
+    "MENU_FILE_CLOSE": {
+        "en_US": "&Close",
+        "zh_CN": "关闭(&C)"
+    },
+    "MENU_SETTING": {
+        "en_US": "&Settings",
+        "zh_CN": "设置(&S)"
+    },
+    "MENU_SETTING_LANGUAGE": {
+        "en_US": "&Language",
+        "zh_CN": "语言(&L)"
+    },
+    "MENU_VIEW": {
+        "en_US": "&View",
+        "zh_CN": "视图(&V)"
+    },
+    "MENU_VIEW_MODULE_LIST": {
+        "en_US": "&Station Modules",
+        "zh_CN": "空间站模块(&S)"
+    },
+    "MENU_VIEW_MODULE_INFO": {
+        "en_US": "&Module Info",
+        "zh_CN": "模块信息(&M)"
+    },
+
+    #Informations
+    "INFO_EFFECT_NEXT_LAUNCH": {
+        "en_US": "The modification will take effect after the next launch.",
+        "zh_CN": "修改将在下次启动后生效."
     }
 }
 
@@ -31,6 +93,8 @@ def set_locale(locale):
     '''
         Set locale..
     '''
+    __LOCALES[locale]
+    global __LOCALE
     __LOCALE = locale
 
 
@@ -39,6 +103,13 @@ def locale():
         Get locale.
     '''
     return __LOCALE
+
+
+def local_dict():
+    '''
+        Get locales and names.
+    '''
+    return __LOCALES.copy()
 
 
 def get_string(key):
@@ -50,4 +121,4 @@ def get_string(key):
         return strings[__LOCALE]
 
     except KeyError:
-        return strings[__DEFRAULT]
+        return strings[__DEFAULT]

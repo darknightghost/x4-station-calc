9d7a23861f8e37bd9dc3fcc5fb3b25fa0ac1408d
wangsiyuan <786663981@qq.com>
2019-01-19 13:07:58 +0800

    UTC:2019-01-19  5:07:58 :

---
+++ Product.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+      Copyright 2019,王思远 <darknightghost.cn@gmail.com>
+      This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+      You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+'''
+    Factions.
+'''
+
+import pathlib
+import json
+import StringTable
+
+__PRODUCTS = {}
+
+
+def __initialize():
+    products_dir = pathlib.Path(__file__).parent / "factions"
+    for fname in factions_dir.glob("*.json"):
+        with open(str(fname)) as f:
+            data = json.loads(f.read())
+            faction = Faction(data)
+            __FACTIONS[faction.id()] = faction
+

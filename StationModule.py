80f325b48c097eb80ced77f194d8fa028ce173b7
wang_siyuan <darknightghost@gmail.com>
2019-02-11 14:20:09 +0800

    UTC:2019-02-11  6:20:09 : station m,odules

--- StationModule.py
+++ StationModule.py
@@ -30,11 +30,6 @@ from Common import *
 __STATION_MODULES = {}
 __STATION_MODULE_TYPE = {}
 '''
-    "Storage":  ["STATION_TYPE_STORAGE", None],
-    "Defence": ["STATION_TYPE_DEFENCE", None],
-    "Habitation": ["STATION_TYPE_HABITATION", None],
-    "Dock": ["STATION_TYPE_DOCK", None],
-    "Other": ["STATION_TYPE_OTHER", None],
     "Venture": ["STATION_TYPE_VENTURE", None],
     "Build": ["STATION_TYPE_BUILD", None]
 '''
@@ -70,6 +65,39 @@ def __registStationModule(typeStr, nameID, cls):
     '''
     global __STATION_MODULE_TYPE
     __STATION_MODULE_TYPE[typeStr] = (nameID, cls)
+
+
+class StationProductInfo:
+    '''
+        Station product info.
+    '''
+
+    @TypeChecker(object, dict)
+    def __init__(self, data):
+        self.__id = data["id"]
+        Product.product(self.__id)
+        self.__amount = data["amount"]
+        if not isinstance(self.__amount, int):
+            raise TypeError("Amount should be an integer.")
+
+    def product(self):
+        '''
+            Get product.
+        '''
+        return Product.product(self.__id)
+
+    def amount(self):
+        '''
+            Get amount.
+        '''
+        return self.__amount
+
+    def __str__(self):
+        return "{\n" \
+                "    product name = %s\n" \
+                "    amount = %d\n" \
+                "}" % (self.product().name(),
+                        self.amount())
 
 
 class __StationModule:
@@ -157,53 +185,207 @@ class __StationModule:
                     )
 
 
+class DefenceModule(__StationModule):
+    '''
+        Defence module.
+    '''
+
+    @TypeChecker(object, dict)
+    def __init__(self, data):
+        super().__init__(data)
+        self.__sLaunchTubes = data["s_launch_tubes"]
+        if not isinstance(self.__sLaunchTubes, int):
+            raise TypeError("Value of s_launch_tubes should be an integer.")
+
+        self.__mLaunchTubes = data["m_launch_tubes"]
+        if not isinstance(self.__mLaunchTubes, int):
+            raise TypeError("Value of m_launch_tubes should be an integer.")
+
+    def sLaunchTubes(self):
+        '''
+            Get number of s launch tubes.
+        '''
+        return self.__sLaunchTubes
+
+    def sLaunchTubes(self):
+        '''
+            Get number of m launch tubes.
+        '''
+        return self.__mLaunchTubes
+
+    def __str__(self):
+        commonStr = addIndent(super().__str__())
+        return "{\n" \
+                "%s,\n" \
+                "    number of s launch tubes = %d,\n" \
+                "    number of m launch tubes = %d\n" \
+                "}" % (
+                        commonStr,
+                        self.__sLaunchTubes,
+                        self.__mLaunchTubes)
+
+
+#Regist type
+__registStationModule("Defence", "STATION_TYPE_DEFENCE", DefenceModule)
+
+
+class DockModule(__StationModule):
+    '''
+        Dock module.
+    '''
+
+    @TypeChecker(object, dict)
+    def __init__(self, data):
+        super().__init__(data)
+        self.__shipStorage = data["ship_storage"]
+        if not isinstance(self.__shipStorage, int):
+            raise TypeError("Value of ship_storage should be an integer.")
+
+        self.__sDocking = data["s_docking"]
+        if not isinstance(self.__sDocking, int):
+            raise TypeError("Value of s_docking should be an integer.")
+
+        self.__mDocking = data["m_docking"]
+        if not isinstance(self.__mDocking, int):
+            raise TypeError("Value of m_docking should be an integer.")
+
+        self.__lXLDocking = data["l_xl_docking"]
+        if not isinstance(self.__lXLDocking, int):
+            raise TypeError("Value of l_xl_docking should be an integer.")
+
+    def shipStorage(self):
+        '''
+            Get ship storage.
+        '''
+        return self.__shipStorage
+
+    def sDocking(self):
+        '''
+            Get number of s docking.
+        '''
+        return self.__sDocking
+
+    def mDocking(self):
+        '''
+            Get number of m docking.
+        '''
+        return self.__mDocking
+
+    def lXLDocking(self):
+        '''
+            Get number of L/XL docking.
+        '''
+        return self.__lXLDocking
+
+    def __str__(self):
+        commonStr = addIndent(super().__str__())
+        return "{\n" \
+                "%s,\n" \
+                "    number of ship storage = %d,\n" \
+                "    number of S docking = %d,\n" \
+                "    number of M docking = %d,\n" \
+                "    number of L/XL ldocking = %d\n" \
+                "}" % (
+                        commonStr,
+                        self.__shipStorage,
+                        self.__sDocking,
+                        self.__mDocking,
+                        self.__lXLDocking)
+
+
+#Regist type
+__registStationModule("Dock", "STATION_TYPE_DOCK", DockModule)
+
+
+class HabitationModule(__StationModule):
+    ''' 
+        Habitation modules.
+    '''
+
+    @TypeChecker(object, dict)
+    def __init__(self, data):
+        super().__init__(data)
+
+        self.__workforceCapacity = data["workforce_capacity"]
+        if not isinstance(self.__workforceCapacity, int):
+            raise TypeError(
+                "Value of workforce_capacity should be an integer.")
+
+        self.__foods = []
+        for f in data["food"]:
+            self.__foods.append(StationProductInfo(f))
+
+    def workforceCapacity(self):
+        '''
+            Get workforce capacity.
+        '''
+        return self.__workforceCapacity
+
+    def foods(self):
+        '''
+            Get foods.
+        '''
+        return self.__foods
+
+    def __str__(self):
+        commonStr = addIndent(super().__str__())
+        foodsStr = "foods = [\n"
+        for f in self.foods():
+            foodsStr += "%s,\n" % (addIndent(str(f)))
+
+        foodsStr = addIndent(foodsStr[:-2] + "\n]")
+        return "{\n" \
+                "%s,\n" \
+                "    workforce capacity = %d,\n" \
+                "%s\n" \
+                "}" % (
+                        commonStr,
+                        self.workforceCapacity(),
+                        foodsStr)
+
+
+#Regist type
+__registStationModule("Habitation", "STATION_TYPE_HABITATION",
+                      HabitationModule)
+
+
+class OtherModule(__StationModule):
+    '''
+        Other modules.
+    '''
+
+    @TypeChecker(object, dict)
+    def __init__(self, data):
+        super().__init__(data)
+
+    def __str__(self):
+        commonStr = addIndent(super().__str__())
+
+        return "{\n" \
+                "%s\n" \
+                "}" % (
+                        commonStr)
+
+
+#Regist type
+__registStationModule("Other", "STATION_TYPE_OTHER", OtherModule)
+
+
 class ProductionModule(__StationModule):
     '''
         Production modules.
     '''
-
-    class ProductInfo:
-        '''
-            Station product info.
-        '''
-
-        @TypeChecker(object, dict)
-        def __init__(self, data):
-            self.__id = data["id"]
-            Product.product(self.__id)
-            self.__amount = data["amount"]
-            if not isinstance(self.__amount, int):
-                raise TypeError("Amount should be an integer.")
-
-        def product(self):
-            '''
-                Get product.
-            '''
-            return Product.product(self.__id)
-
-        def amount(self):
-            '''
-                Get amount.
-            '''
-            return self.__amount
-
-        def __str__(self):
-            return "{\n" \
-                    "    product name = %s\n" \
-                    "    amount = %d\n" \
-                    "}" % (self.product().name(),
-                            self.amount())
 
     @TypeChecker(object, dict)
     def __init__(self, data):
         super().__init__(data)
         self.__products = []
         for p in data["products"]:
-            self.__products.append(self.ProductInfo(p))
+            self.__products.append(StationProductInfo(p))
 
         self.__resources = []
         for r in data["resources"]:
-            self.__resources.append(self.ProductInfo(r))
+            self.__resources.append(StationProductInfo(r))
 
     def products(self):
         '''
@@ -233,7 +415,7 @@ class ProductionModule(__StationModule):
         return "{\n" \
                 "%s,\n" \
                 "%s,\n" \
-                "%s,\n" \
+                "%s\n" \
                 "}" % (
                         commonStr,
                         resourcesStr,
@@ -243,6 +425,57 @@ class ProductionModule(__StationModule):
 #Regist type
 __registStationModule("Production", "STATION_TYPE_PRODUCTION",
                       ProductionModule)
+
+
+class StorageModule(__StationModule):
+    '''
+        Storage modules.
+    '''
+
+    @TypeChecker(object, dict)
+    def __init__(self, data):
+        super().__init__(data)
+
+        self.__storageType = data["storage_type"]
+
+        if not isinstance(self.__storageType, str):
+            raise TypeError("Type of storage_type must be string.")
+
+        if self.__storageType not in ("Solid", "Container", "Liquid"):
+            raise ValueError("Illegale storage type \"%s\"." \
+                %(self.__storageType))
+
+        self.__storageCapacity = data["storage_capacity"]
+        if not isinstance(self.__storageCapacity, int):
+            raise TypeError("Type of storage_capacity must be integer.")
+
+    def storageType(self):
+        '''
+            Get storage type.
+        '''
+        return self.__storageType
+
+    def storageCapacity(self):
+        '''
+            Get storage capacity.
+        '''
+        return self.__storageCapacity
+
+    def __str__(self):
+        commonStr = addIndent(super().__str__())
+
+        return "{\n" \
+                "%s,\n" \
+                "    storage type = %s,\n" \
+                "    storage capacity = %d\n" \
+                "}" % (
+                        commonStr,
+                        self.storageType(),
+                        self.storageCapacity())
+
+
+#Regist type
+__registStationModule("Storage", "STATION_TYPE_STORAGE", StorageModule)
 
 __initialize()
 

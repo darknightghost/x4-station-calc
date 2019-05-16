ea13a043524feaf51b2b9315fee88a98fa6796bc
Wang Siyuan <darknightghost.cn@gmail.com>
2019-05-16 21:28:04 +0800

    Fix bugs.

--- Common/__init__.py
+++ Common/__init__.py
@@ -21,7 +21,7 @@ import inspect
 import functools
 from distutils.version import StrictVersion
 
-VERSION = StrictVersion("0.0.5")
+VERSION = StrictVersion("0.0.6")
 RECORD_VERSION = StrictVersion("0.0.2")
 
 import Common

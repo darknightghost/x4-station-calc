213605b89a78a3adb5ae15f47fe7486b8bd380af
wang_siyuan <darknightghost@gmail.com>
2019-12-21 19:24:45 +0800

    UTC:2019-12-21 11:24:45 : game path

--- include/game_data/game_data.h
+++ include/game_data/game_data.h
@@ -1,1 +1,50 @@
+#pragma once
 
+#include <QtCore/QMap>
+#include <QtCore/QObject>
+#include <QtCore/QReadWriteLock>
+
+#include <design_model/singleton.h>
+
+/**
+ * @brief   String table.
+ */
+class GameData : public QObject, public Singleton<GameData> {
+    Q_OBJECT
+    SIGNLETON_OBJECT(GameData)
+  private:
+  protected:
+    /**
+     * @brief   Constructor.
+     */
+    GameData();
+
+  public:
+    /**
+     * @brief		Check path of game.
+     *
+     * @param[in]	path	Path of the game.
+     *
+     * @return		True if the path of game is available, otherwise returns
+     * false.
+     *
+     */
+    static bool checkGamePath(const QString &path);
+
+    /**
+     * @brief		Check path of game.
+     *
+     * @param[in]	path	Path of the game.
+     *
+     * @return		True if the path of game is available, otherwise returns
+     * false.
+     *
+     */
+    static bool setGamePath(const QString &path);
+
+  public:
+    /**
+     * @brief Destructor.
+     */
+    virtual ~GameData();
+};
--- source/game_data/game_data.cc
+++ source/game_data/game_data.cc
@@ -1,1 +1,42 @@
+#include <config.h>
+#include <game_data/game_data.h>
+#include <locale/string_table.h>
 
+/**
+ * @brief   Constructor.
+ */
+GameData::GameData() {}
+
+/**
+ * @brief		Check path of game.
+ *
+ * @param[in]	path	Path of the game.
+ *
+ * @return		True if the path of game is available, otherwise returns
+ * false.
+ *
+ */
+bool GameData::checkGamePath(const QString &path) {}
+
+/**
+ * @brief		Check path of game.
+ *
+ * @param[in]	path	Path of the game.
+ *
+ * @return		True if the path of game is available, otherwise returns
+ * false.
+ *
+ */
+bool GameData::setGamePath(const QString &path)
+{
+    if (! checkGamePath(path)) {
+        return false;
+    }
+    Config::instance()->setString("/gamePath", path);
+    return true;
+}
+
+/**
+ * @brief Destructor.
+ */
+GameData::~GameData() {}
